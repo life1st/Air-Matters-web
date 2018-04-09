099a9219534f8c8867e8c16530c1d6d60a8009fd
life1st <jiaoyangdev@gmail.com>
2018-04-09 15:52:06 +0800

    update some function.

--- client/demo/main.js
+++ client/demo/main.js
@@ -1,10 +1,51 @@
-function getAQI() {
+function getAQIList() {
   // let url = 'https://air-matters.com/js/data/map_en_aqi_us.js'
-  let url = 'http://localhost:3000/'
-  $.get(url).then(res => {
+  let url = 'http://localhost:3000/all'
+  return axios.get(url).then(res => {
     console.log('getAQI:>>>', res)
+    // return new Promise(resolve => resolve(res))
+    return res.data
   })
 }
+function getCityData(config) {
+  let url = `http://localhost:3000/city`
+  // return axios.get()
+}
+
+function renderlist(cityList) {
+  let itemTpl = (name, id, key) =>
+    `<li data-key="${key}" data-id="${id}" data-name="${name}">
+        城市：${name}
+       </li>
+      `
+
+  let tpl = ''
+  cityList.forEach(city => {
+    tpl += itemTpl(city.name, city.place_id, city.url_key)
+  })
+  let cityWrap = document.querySelector('.city-display')
+  cityWrap.innerHTML = tpl
+  addEvent()
+}
+function addEvent() {
+  let list = document.querySelector('.city-display')
+  list.addEventListener('click', (event) => {
+    console.log(event)
+    let target = event.target
+    let key = target.dataset.key
+    let id = target.dataset.id
+    let name = target.dataset.name
+    console.log(key, id, name)
+
+  })
+}
+window.addEventListener('load', () => {
+  getAQIList()
+    .then((cityList) => {
+      renderlist(cityList)
+    })
+})
+
 
 // let url = 'https://air-matters.com/js/data/map_en_aqi_us.js'
 // let method = 'GET'
@@ -14,7 +55,3 @@ function getAQI() {
 // xhr.onreadystatechange = function (response) {
 //   console.log('xhr>>>', response)
 // }
-
-window.addEventListener('load', () => {
-  getAQI()
-})
--- server/app.js
+++ server/app.js
@@ -6,23 +6,35 @@ const cors = require('@koa/cors')
 
 // CORS
 app.use(cors())
-
 // router
-router.get('/', getCitys)
-  .get('/city/:placeId', getCity)
+router.get('/all', getCitys)
+  .get('/city', getCity)
+
 async function getCitys(ctx) {
   let url = 'https://air-matters.com/js/data/map_en_aqi_us.js'
   let res = await axios.get(url)
   ctx.status = 200
-  ctx.type = 'html'
   ctx.body = res.data
 }
 async function getCity(ctx) {
-  let id = ctx.params.placeId
+  let parmas = ctx.query
+  const BASE_URL = 'https://app.air-matters.com/place/'
+  /* + `china/beijing/en/aqi_us/8733f5dd`*/
+  /*   `${country}/${city}/${LANG}/${benchMark}/${link}*/
+  let country = parmas.country
+  let city = parmas.city
+  let link = parmas.link
+  let lang = parmas.lang || 'en'
+  let benchMark = parmas.benchmark || 'aqi_us'
+  let place_URL = (country, city, LANG, benchMark, link) => `${BASE_URL}${country}/${city}/${LANG}/${benchMark}/${link}`
 
-  let url = 'https://app.air-matters.com/place/china/beijing/zh_cn/aqi_us/8733f5dd'
+  let res = await axios.get(place_URL(country, city, lang, benchMark, link))
+    .then(res => {
+      let data = res.data
+      return data
+    })
   ctx.status = 200
-  ctx.body = id
+  ctx.body = res
 }
 app.use(router.routes())
 console.log('app listen: 3000')
