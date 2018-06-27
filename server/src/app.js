fb97a5a7c57b28f1d2351d1570df8ce25d568531
Yang <jiaoyangdev@gmail.com>
2018-06-27 14:38:42 +0800

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,9 @@ const app = new koa()
 const cors = require('@koa/cors')
 const router = require('./router/index')
 // CORS
-app.use(cors())
+app.use(cors({
+  'Access-Control-Allow-Origin': 'air.life1st.me'
+}))
 // router
 app.use(router.routes())
 console.log('app listen: 3000')
